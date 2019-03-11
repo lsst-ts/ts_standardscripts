72a0b1166219d87f9d92a8bff8f1f6688cfcf366
Tiago Ribeiro <tiago.astro@gmail.com>
2019-03-11 15:39:46 -0700

    Implemented most comments and requests from code review.

--- python/lsst/ts/standardscripts/auxtel/detector_characterization/get_std_flat_dataset.py
+++ python/lsst/ts/standardscripts/auxtel/detector_characterization/get_std_flat_dataset.py
@@ -30,18 +30,24 @@ import SALPY_ATSpectrograph
 
 
 class ATGetStdFlatDataset(scriptqueue.BaseScript):
-    """Implement script to get sensor characterization data. The definition is spelled out in
-    https://jira.lsstcorp.org/browse/CAP-203 and https://jira.lsstcorp.org/browse/CAP-206.
+    """Implement script to get sensor characterization data.
+
+    The definition is spelled out in https://jira.lsstcorp.org/browse/CAP-203
+    and https://jira.lsstcorp.org/browse/CAP-206.
 
     Basically, this script will:
 
-    1 - Take a set `nd` (default=10) dark images (shutter closed) with `td` (default=400s) exposure time
-    2 - Take a set of `nb` (default=10) bias
+    1 - Take a set `n_dark` (default=10) dark images (shutter closed) with
+        `t_dark` (default=400s) exposure time
+    2 - Take a set of `n_bias` (default=10) bias
     3 - Flat field data:
-        - Take a set of pairs of flat fields at a set of approximately logarithmically spaced intensity
-        levels starting at 500 DN and increasing by a factor of 2 (i.e. 500, 1000, 2000, ...).
-        The exact levels are not important, but they must be well known; both the flux level and
-        shutter time must be well measured (if shutter is opened before/closed after the lamp
+        - Take a set of pairs of flat fields at a set of approximately
+        logarithmically spaced intensity
+        levels starting at 500 DN and increasing by a factor of 2 (i.e. 500,
+        1000, 2000, ...).
+        The exact levels are not important, but they must be well known; both
+        the flux level and shutter time must be well measured (if shutter is
+        opened before/closed after the lamp
         is turned on then the shutter time need not be well measured).
         - Take a set of `nb` biases after the entire flat sequence is complete
 
@@ -51,11 +57,10 @@ class ATGetStdFlatDataset(scriptqueue.BaseScript):
         Index of Script SAL component.
     """
 
-    def __init__(self, index,
-                 descr="Take Flat field sensor characterization data."):
+    def __init__(self, index):
 
         super().__init__(index=index,
-                         descr=descr,
+                         descr="Take Flat field sensor characterization data.",
                          remotes_dict=dict(atcam=salobj.Remote(SALPY_ATCamera,
                                                                include=["takeImages",
                                                                         "endReadout"]),
@@ -64,30 +69,32 @@ class ATGetStdFlatDataset(scriptqueue.BaseScript):
                                                                          "changeDisperser",
                                                                          "moveLinearStage"])))
 
-        # Had to increase this timeout to accommodate some issue with the camera.
-        self.cmd_timeout = 120.
-        self.read_out_time = 2.
-        self.n_dark = 10
-        self.t_dark = 400.
-        self.n_bias = 10
-        self.n_flat = 2
-        self.flat_base_exptime = 0.5
-        self.flat_dn_range = np.array([1., 2., 4., 8., 16., 32., 64., 128.])
+        self.cmd_timeout = 30.
+        self.end_readout_timeout = 120.
+
+        self.read_out_time = None
+        self.n_dark = None
+        self.t_dark = None
+        self.n_bias = None
+        self.n_flat = None
+        self.flat_base_exptime = None
+        self.flat_dn_range = None
         self.filter = None
         self.grating = None
         self.linear_stage = None
 
-        # FIXME: Get this parameter from the camera configuration once late joiner is working
+        # FIXME: Get this parameter from the camera configuration once late
+        # joiner is working
         # on the open network.
         self.maximum_exp_time = 401.  # Maximum exposure time in seconds.
 
     async def configure(self,
-                        n_dark=None,
-                        t_dark=None,
-                        n_bias=None,
-                        n_flat=None,
-                        flat_base_exptime=None,
-                        flat_dn_range=None,
+                        n_dark=10,
+                        t_dark=400.,
+                        n_bias=10,
+                        n_flat=2,
+                        flat_base_exptime=0.5,
+                        flat_dn_range=(1., 2., 4., 8., 16., 32., 64., 128.),
                         filter_id=None,
                         grating_id=None,
                         linear_stage=None,
@@ -96,61 +103,60 @@ class ATGetStdFlatDataset(scriptqueue.BaseScript):
 
         Parameters
         ----------
-        n_dark : `int`
-            Number of dark images to be taken (default=10).
-        t_dark : `float`
-            Exposure time for the dark images in seconds (default=400s).
-        n_bias : `int`
-            Number of bias images to be taken (default=10).
-        n_flat : `int`
+        n_dark : `int` (optional)
+            Number of dark images to be taken.
+        t_dark : `float` (optional)
+            Exposure time for the dark images in seconds.
+        n_bias : `int` (optional)
+            Number of bias images to be taken.
+        n_flat : `int` (optional)
             Number of flat field images to take for each exposure time.
-        flat_base_exptime : `float`
+        flat_base_exptime : `float` (optional)
             The exposure time for the first flat field image set (in seconds).
-        flat_dn_range : `list(float)`
-            List of values that multiply the `flat_base_exptime`. For instance, if you want to take
-            two sets of flat with exposure times `flat_base_exptime, 2*flat_base_exptime, 4*flat_dn_range`,
-            `flat_dn_range = [1., 2., 4.]`. Default is [1., 2., 4., 8., 16., 32., 64.].
-        filter_id : `int`
-            Id of the filter to use in the ATSpectrograph. If `None` (default) will ignore the setup step.
-        grating_id : `int`
-            Id of the grating to use in the ATSpectrograph. If `None` (default) will ignore the setup step.
-        linear_stage : `float`
-            Position of the linear stage in the ATSpectrograph. If `None` (default) will ignore the
+        flat_dn_range : `list(float)` (optional)
+            List of values that multiply the `flat_base_exptime`. For instance,
+            if you want to take two sets of flat with exposure times
+            `flat_base_exptime, 2*flat_base_exptime, 4*flat_dn_range`,
+            `flat_dn_range = [1., 2., 4.]`.
+        filter_id : `int` (optional)
+            Id of the filter to use in the ATSpectrograph. If `None` (default)
+            will ignore the setup step.
+        grating_id : `int` (optional)
+            Id of the grating to use in the ATSpectrograph. If `None`
+            (default) will ignore the setup step.
+        linear_stage : `float` (optional)
+            Position of the linear stage in the ATSpectrograph. If `None`
+            (default) will ignore the
             setup step.
-        read_out_time :`float`
-            The read out time of the camera, in seconds. Used to estimate script duration.
+        read_out_time :`float` (optional)
+            The read out time of the camera, in seconds. Used to estimate
+            script duration.
 
         """
 
-        if n_dark is not None and n_dark > 0:
-            self.n_dark = n_dark
-        elif n_dark is not None:
+        if int(n_dark) <= 0:
             raise RuntimeError(f"Number of dark frames must be larger than 0, got {n_dark}")
+        self.n_dark = int(n_dark)
 
-        if t_dark is not None and 0. < t_dark <= self.maximum_exp_time:
-            self.t_dark = t_dark
-        elif n_dark is not None:
+        if not (0. < float(t_dark) <= self.maximum_exp_time):
             raise RuntimeError(f"Dark exptime must be in the range "
                                f"(0.,{self.maximum_exp_time}], got {t_dark}")
+        self.t_dark = float(t_dark)
 
-        if n_bias is not None and n_bias > 0:
-            self.n_bias = n_bias
-        elif n_bias is not None:
+        if int(n_bias) <= 0:
             raise RuntimeError(f"Number of bias frames must be larger than 0, got {n_bias}")
+        self.n_bias = int(n_bias)
 
-        if n_flat is not None and n_flat > 0:
-            self.n_flat = n_flat
-        elif n_flat is not None:
+        if int(n_flat) <= 0:
             raise RuntimeError(f"Number of flat field frames must be larger than 0, got {n_flat}")
+        self.n_flat = int(n_flat)
 
-        if flat_base_exptime is not None and flat_base_exptime > 0.:
-            self.flat_base_exptime = flat_base_exptime
-        elif flat_base_exptime is not None:
+        if float(flat_base_exptime) < 0.:
             raise RuntimeError(f"Base flat field exposure time must be larger than 0., "
-                               f"got {flat_base_exptime}")
+                               f"got {flat_base_exptime}.")
+        self.flat_base_exptime = float(flat_base_exptime)
 
-        if flat_dn_range is not None:
-            self.flat_dn_range = np.array(flat_dn_range, dtype=float)
+        self.flat_dn_range = np.array(flat_dn_range, dtype=float)
 
         if np.any(self.flat_dn_range <= 0.):
             n_bad = len(np.where(self.flat_dn_range <= 0.)[0])
@@ -162,17 +168,14 @@ class ATGetStdFlatDataset(scriptqueue.BaseScript):
             raise RuntimeError(f"Flat field maximum exposure time {larger_flat_exptime}s "
                                f"above maximum allowed {self.maximum_exp_time}s.")
 
-        if filter_id is not None:
-            self.filter = filter_id
+        self.filter = filter_id
 
-        if grating_id is not None:
-            self.grating = grating_id
+        self.grating = grating_id
 
-        if linear_stage is not None:
-            self.linear_stage = linear_stage
+        self.linear_stage = linear_stage
 
-        if read_out_time > 0.:
-            self.read_out_time = read_out_time
+        if float(read_out_time) > 0.:
+            self.read_out_time = float(read_out_time)
         else:
             self.log.warning(f"Read out time must be larger than 0., got {read_out_time}. "
                              f"Using default value, {self.read_out_time}s.")
@@ -186,7 +189,7 @@ class ATGetStdFlatDataset(scriptqueue.BaseScript):
         if self.filter is not None:
             await self.checkpoint(f"Configuring ATSpec filter; {self.filter}")
             self.atspec.cmd_changeFilter.set(filter=self.filter)
-            await self.self.atspec.cmd_changeFilter.start(timeout=self.cmd_timeout)
+            await self.atspec.cmd_changeFilter.start(timeout=self.cmd_timeout)
 
         if self.grating is not None:
             await self.checkpoint(f"Configuring ATSpec grating; {self.grating}")
@@ -196,7 +199,7 @@ class ATGetStdFlatDataset(scriptqueue.BaseScript):
         if self.linear_stage is not None:
             await self.checkpoint(f"Configuring ATSpec linear stage; {self.linear_stage}")
             self.atspec.cmd_moveLinearStage.set(distanceFromHome=self.linear_stage)
-            await self.atspec.cmd_moveLinearStage.start()
+            await self.atspec.cmd_moveLinearStage.start(timeout=self.cmd_timeout)
 
         await self.take_dark_sequence()
 
@@ -209,9 +212,9 @@ class ATGetStdFlatDataset(scriptqueue.BaseScript):
         await self.checkpoint("done")
 
     async def take_dark_sequence(self):
-        """A coroutine to take the intended sequence of darks.
+        """Take the intended sequence of darks.
         """
-        self.log.debug("Taking dark sequence...")
+        self.log.info(f"Taking {self.n_dark} dark images...")
 
         for i in range(self.n_dark):
             await self.take_image(exp_time=self.t_dark,
@@ -219,9 +222,9 @@ class ATGetStdFlatDataset(scriptqueue.BaseScript):
                                   image_seq_name=f"dark_{i+1:04d}_{self.n_dark:04d}")
 
     async def take_bias_sequence(self):
-        """A coroutine to take the intended sequence of bias.
+        """Take the intended sequence of bias.
         """
-        self.log.debug("Taking bias sequence...")
+        self.log.info(f"Taking {self.n_bias} bias images...")
 
         for i in range(self.n_bias):
             await self.take_image(exp_time=0.,
@@ -229,9 +232,9 @@ class ATGetStdFlatDataset(scriptqueue.BaseScript):
                                   image_seq_name=f"bias_{i+1:04d}_{self.n_bias:04d}")
 
     async def take_flat_sequence(self):
-        """A coroutine to take the intended sequence of flats.
+        """Take the intended sequence of flats.
         """
-        self.log.debug("Taking flat sequence...")
+        self.log.info("Taking flat-field sequence...")
 
         for i in range(len(self.flat_dn_range)):
             for j in range(self.n_flat):
@@ -243,7 +246,10 @@ class ATGetStdFlatDataset(scriptqueue.BaseScript):
                                                      f"{self.n_flat:04d}")
 
     async def take_image(self, exp_time, shutter, image_seq_name):
-        """Coroutine to take images.
+        """Encapsulates the take image routine.
+
+        This basically consists of configuring and sending a takeImages
+        command to the camera and waiting for a endReadout event.
 
         Parameters
         ----------
@@ -258,12 +264,15 @@ class ATGetStdFlatDataset(scriptqueue.BaseScript):
         -------
 
         """
+        # FIXME: Current version of ATCamera software is not set up to take
+        # images with numImages > 1, so this is fixed at 1 for now and we
+        # loop through any set of images we want to take. (2019/03/11)
         self.atcam.cmd_takeImages.set(numImages=1,
                                       expTime=exp_time,
                                       shutter=shutter,
                                       imageSequenceName=str(image_seq_name))
 
-        timeout = self.read_out_time + self.cmd_timeout
+        timeout = self.read_out_time + self.cmd_timeout + self.end_readout_timeout
 
         end_readout_coro = self.atcam.evt_endReadout.next(flush=True,
                                                           timeout=timeout)
@@ -274,6 +283,10 @@ class ATGetStdFlatDataset(scriptqueue.BaseScript):
         await end_readout_coro
 
     def set_metadata(self, metadata):
-        metadata.duration = self.n_dark * (
-            self.read_out_time + self.t_dark) + 2. * self.n_bias * self.read_out_time + np.sum(
-            self.n_flat * self.flat_dn_range * (self.read_out_time + self.flat_base_exptime))
+        dark_time = self.n_dark * (self.read_out_time + self.t_dark)
+        # Note, bias is taken twice, once at the beginning then at the end of
+        # the sequence.
+        bias_time = 2. * self.n_bias * self.read_out_time
+        flat_time = np.sum(self.n_flat * self.flat_dn_range * (self.read_out_time +
+                                                               self.flat_base_exptime))
+        metadata.duration = dark_time + bias_time + flat_time
--- tests/auxtel/detector_characterization/test_std_flat_dataset.py
+++ tests/auxtel/detector_characterization/test_std_flat_dataset.py
@@ -28,26 +28,39 @@ class Harness:
 
         salobj.test_utils.set_random_lsst_dds_domain()
 
-        self.script = ATGetStdFlatDataset(index=self.index, descr='Test ATGetStdFlatDataset')
+        self.script = ATGetStdFlatDataset(index=self.index)
 
         # Adds controller to Test
         self.at_cam = salobj.Controller(SALPY_ATCamera)
         self.at_spec = salobj.Controller(SALPY_ATSpectrograph)
 
-        self.nbias = 0
-        self.ndark = 0
-        self.nflat = 0
+        self.n_bias = 0
+        self.n_dark = 0
+        self.n_flat = 0
+
+        self.filter = None
+        self.grating = None
+        self.linear_stage = None
 
     async def cmd_take_images_callback(self, id_data):
         if "bias" in id_data.data.imageSequenceName:
-            self.nbias += 1
+            self.n_bias += 1
         elif "dark" in id_data.data.imageSequenceName:
-            self.ndark += 1
+            self.n_dark += 1
         elif "flat" in id_data.data.imageSequenceName:
-            self.nflat += 1
+            self.n_flat += 1
         await asyncio.sleep(self.script.read_out_time)
 
         self.at_cam.evt_endReadout.put(self.at_cam.evt_endReadout.DataType())
+
+    async def cmd_change_filter_callback(self, id_data):
+        self.filter = id_data.data.filter
+
+    async def cmd_change_grating_callback(self, id_data):
+        self.grating = id_data.data.disperser
+
+    async def cmd_move_linear_stage_callback(self, id_data):
+        self.linear_stage = id_data.data.distanceFromHome
 
 
 class TestATGetStdFlatDataset(unittest.TestCase):
@@ -56,10 +69,18 @@ class TestATGetStdFlatDataset(unittest.TestCase):
         async def doit():
             harness = Harness()
 
-            # Adds callback to take image command
             harness.at_cam.cmd_takeImages.callback = harness.cmd_take_images_callback
+            harness.at_spec.cmd_changeFilter.callback = harness.cmd_change_filter_callback
+            harness.at_spec.cmd_changeDisperser.callback = harness.cmd_change_grating_callback
+            harness.at_spec.cmd_moveLinearStage.callback = harness.cmd_move_linear_stage_callback
 
+            # Make sure configuration works with no parameter
             await harness.script.configure()
+
+            # Now configure the spectrograph
+            await harness.script.configure(filter_id=0,
+                                           grating_id=3,
+                                           linear_stage=10.)
 
             harness.script.set_state(ScriptState.RUNNING)
 
@@ -67,9 +88,12 @@ class TestATGetStdFlatDataset(unittest.TestCase):
             await harness.script._run_task
             harness.script.set_state(ScriptState.ENDING)
 
-            self.assertEqual(harness.nbias, harness.script.n_bias*2)
-            self.assertEqual(harness.ndark, harness.script.n_dark)
-            self.assertEqual(harness.nflat, len(harness.script.flat_dn_range)*harness.script.n_flat)
+            self.assertEqual(harness.n_bias, harness.script.n_bias * 2)
+            self.assertEqual(harness.n_dark, harness.script.n_dark)
+            self.assertEqual(harness.n_flat, len(harness.script.flat_dn_range) * harness.script.n_flat)
+            self.assertEqual(harness.filter, 0)
+            self.assertEqual(harness.grating, 3)
+            self.assertEqual(harness.linear_stage, 10.)
 
         stream_handler = logging.StreamHandler(sys.stdout)
         logger.addHandler(stream_handler)
