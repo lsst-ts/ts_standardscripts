8f6c7b7b757f7b18fa92abb9a2cd50de6366af0e
Colin Winslow <cwinslow@lsst.org>
2019-05-31 14:08:42 -0400

    fault state tests

--- tests/test_attcs.py
+++ tests/test_attcs.py
@@ -22,6 +22,10 @@ class Harness:
         self.test_index = next(index_gen)
         test_utils.set_random_lsst_dds_domain()
 
+        self.atmcs = salobj.Controller(SALPY_ATMCS)
+        self.atptg = salobj.Controller(SALPY_ATPtg)
+
+        # ATMCSRem = salobj.Remote(self.atmcs.domain) # uncomment when we switch to new salobj
         ATMCSRem = salobj.Remote(SALPY_ATMCS)
         ATAOSRem = salobj.Remote(SALPY_ATAOS)
         ATPneumaticsRem = salobj.Remote(SALPY_ATPneumatics)
@@ -30,20 +34,18 @@ class Harness:
         ATDomeTrajRem = None
 
         self.attcs = ATTCS(ATMCSRem, ATAOSRem, ATPneumaticsRem,
-                        ATHexapodRem, ATDomeRem, ATDomeTrajRem)
+                           ATHexapodRem, ATDomeRem, ATDomeTrajRem)
 
-        self.atmcs = salobj.Controller(SALPY_ATMCS)
-        self.atptg = salobj.Controller(SALPY_ATPtg)
         self.atptg.cmd_raDecTarget.callback = self.cmd_raDecTarget_callback
 
     async def cmd_raDecTarget_callback(self, id_data):
-        """fake slew waits 5 seconds, then reports all axes 
+        """fake slew waits 5 seconds, then reports all axes
            in position. Doesdoes not simulate the actual slew."""
         asyncio.sleep(5)
         self.atmcs.evt_allAxesInPosition.put()
 
 
-class testATTCS(unittest.TestCase):        
+class testATTCS(unittest.TestCase):
 
     def test_slew(self):
         test_utils.set_random_lsst_dds_domain()
@@ -51,16 +53,32 @@ class testATTCS(unittest.TestCase):
         async def runtest():
             harness = Harness()
             harness.attcs.slew(45., 45.)
-            slewResult = await harness.attcs.atmcs.tel_mountEncoders.next(flush=False, timeout=0)
+            slewResult = await harness.attcs.atmcs.tel_mountEncoders.next(flush=False, timeout=10)
             assert isclose(slewResult.elevationCalculatedAngle, 45.0, rel_tol=0.03)
             assert isclose(slewResult.azimuthCalculatedAngle, 45.0, rel_tol=0.03)
         asyncio.get_event_loop().run_until_complete(runtest())
 
     def test_fails_when_atptg_faults(self):
-        pass
+        test_utils.set_random_lsst_dds_domain()
+
+        async def runtest():
+            harness = Harness()
+            self.atptg.evt_summaryState.set_put(summaryState=salobj.State.FAULT)
+            await harness.attcs.slew(45., 45.)
+
+        with self.assertRaises(RuntimeError):
+            asyncio.get_event_loop().run_until_complete(runtest)
 
     def test_fails_when_atmcs_faults(self):
-        pass
+        test_utils.set_random_lsst_dds_domain()
+
+        async def runtest():
+            harness = Harness()
+            self.atmcs.evt_summaryState.set_put(summaryState=salobj.State.FAULT)
+            await harness.attcs.slew(45., 45.)
+
+        with self.assertRaises(RuntimeError):
+            asyncio.get_event_loop().run_until_complete(runtest)
 
 
 if __name__ == '__main__':
