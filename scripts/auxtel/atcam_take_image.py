0b6920168e7704770c14cd915cc31c4a64e504d6
Russell Owen <rowen@uw.edu>
2019-06-10 20:56:02 -0700

    Simplify atcam_take_image.py a bit
    by importing the script from auxtel, not the module

--- scripts/auxtel/atcam_take_image.py
+++ scripts/auxtel/atcam_take_image.py
@@ -1,4 +1,4 @@
-from lsst.ts.standardscripts.auxtel.atcam_take_image import ATCamTakeImage
+from lsst.ts.standardscripts.auxtel import ATCamTakeImage
 
 if __name__ == '__main__':
     ATCamTakeImage.main()
