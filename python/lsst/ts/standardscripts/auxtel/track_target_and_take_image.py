03e4a4d20e642f414d3ef903b5eafd884668669c
Tiago Ribeiro <tribeiro@lsst.org>
2022-10-07 16:20:59 -0700

    In ``python/lsst/ts/standardscripts/auxtel/track_target_and_take_image.py``, add a new configuration parameter "filter_suffix" to allow appending strings to the filter name.

--- python/lsst/ts/standardscripts/auxtel/track_target_and_take_image.py
+++ python/lsst/ts/standardscripts/auxtel/track_target_and_take_image.py
@@ -79,6 +79,10 @@ properties:
     description: Prefix to add to filter name.
     type: string
     default: ""
+  filter_suffix:
+    description: Suffix to append to filter name.
+    type: string
+    default: ""
 required:
   - grating
 additionalProperties: false
@@ -126,7 +130,7 @@ additionalProperties: false
         setup_atspec_task = asyncio.create_task(
             self.latiss.setup_atspec(
                 grating=self.grating[0],
-                filter=f"{self.config.filter_prefix}{self.band_filter[0]}",
+                filter=f"{self.config.filter_prefix}{self.band_filter[0]}{self.config.filter_suffix}",
             )
         )
 
@@ -196,7 +200,7 @@ additionalProperties: false
                     exptime=exptime,
                     n=1,
                     group_id=self.group_id,
-                    filter=f"{self.config.filter_prefix}{band_filter}",
+                    filter=f"{self.config.filter_prefix}{band_filter}{self.config.filter_suffix}",
                     grating=grating,
                     reason=self.config.reason,
                     program=self.config.program,
