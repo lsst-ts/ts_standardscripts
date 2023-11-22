93ac7a5e9586e42b57c07f5c7a5ece93ef881253
Dan <dan.weatherill@cantab.net>
2023-11-22 12:03:42 +0000

    complete ATCalsys power on script

--- python/lsst/ts/standardscripts/auxtel/calibrations/power_on_newcalsys.py
+++ python/lsst/ts/standardscripts/auxtel/calibrations/power_on_newcalsys.py
@@ -1,73 +1,55 @@
 import asyncio
 import yaml
-from lsst.ts import salobj
+from lsst.ts.salobj import BaseScript
 from lsst.ts.observatory.control.auxtel.atcalsys import ATCalsys
+from lsst.ts.observatory.control.base_calsys import CalsysScriptIntention
+import astropy.units as un
 
-class PowerOnATCalSys(salobj.BaseScript):
-    """ Powers on teh ATCalsys ready for daily (or monochromated) flat field calibrations """
+class PowerOnATCalSysNew(salobj.BaseScript):
+    """ Powers on AT calsys ready for calibrations """
 
     def __init__(self, index, add_remotes: bool=True):
         super().__init__(index=index, descr="Power On AT Calibration System")
-        self._calsys = ATCalsys(log=self.log)
+        self._calsys = ATCalsys(CalsysScriptIntention.TURN_ON, log=self.log)
 
     @classmethod
     def get_schema(cls):
         schema_yaml = """
-                $schema: http://json-schema.org/draft-07/schema#
-            $id: https://github.com/lsst-ts/ts_standardscripts/auxtel/calibrations/power_on_atcalsys.yaml
-            title: PowerOnATCalSys v1
-            description: Configuration for PowerOnATCalSys.
-              Each attribute can be specified as a scalar or array.
-              All arrays must have the same length (one item per image).
+            $schema: http://json-schema.org/draft-07/schema#
+            $id: https://github.com/lsst-ts/ts_standardscripts/auxtel/calibrations/power_on_newcalsys.yaml
+            title: PowerOnATCalSysNew v1
+            description: Configuration for PowerOnATCalSysNew.
+              Each attribute is nullable, in which case the ATCalsys helper class will decide a sensible
+              default, or load current defaults from other sources
             type: object
             properties:
               chiller_temperature:
                 description: Set temperature for the chiller
-                type: number
+                type: [number, null]
                 default: 20
                 minimum: 10
 
               whitelight_power:
-                description: White light power.
-                type: number
+                description: White light power (Units of Watts).
+                type: [number, null]
                 default: 910
                 minimum: 0
-
-              wavelength:
-                description: Wavelength (nm). 0 nm is for white light.
-                type: number
-                default: 0
-                minimum: 0
-
-              grating_type:
-                description: Grating type for each image. The choices are 0=blue, 1=red, 2=mirror.
-                type: integer
-                enum: [0, 1, 2]
-                default: 2
-
-              entrance_slit_width:
-                description: Width of the monochrometer entrance slit (mm)
-                type: number
-                minimum: 0
-                default: 7
-
-              exit_slit_width:
-                description: Width of the monochromator entrance slit (mm)
-                type: number
-                minimum: 0
-                default: 7
-
-              use_atmonochromator:
-                description: Is the monochromator available and can be configured?
-                    If False, the monochromator will be left as it is.
-                    If True, the monochromator will be configured for white light.
-        type: boolean
-                default: false
 
             additionalProperties: false
         """
         return yaml.safe_load(schema_yaml)
 
     async def configure(self, config):
-        
-        pass
+        if config.chiller_temperature is not None:
+            self._calsys.CHILLER_SETPOINT_TEMP = config.chiller_temperature << un.deg_C
+        if config.whitelight_power is not None:
+            self._calsys.WHITELIGHT_POWER = config.whitelight_power << un.W
+
+    def set_metadata(self, metadata):
+        metadata.duration = self._calsys.script_time_estimate_s
+
+    async def run(self):
+        await self._calsys.enable()
+        await self.checkpoint("all components enabled, beginning power up sequence")
+        await self._calsys.power_sequence_run(self)
+
