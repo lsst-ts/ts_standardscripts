133cc9c2626ee354bee32fb8f2bc89244c3b7033
Marco Rocchietti <marco.rocchietti@unc.edu.ar>
2024-10-17 11:49:36 -0300

    Use the new method ATCS.assert_ataos_corrections_enabled in auxtel scripts
    Updated scripts:
    
    - ``latiss_take_sequence.py``
    - ``offset_ataos.py``
    - ``track_target_and_take_image.py``

--- python/lsst/ts/standardscripts/auxtel/latiss_take_sequence.py
+++ python/lsst/ts/standardscripts/auxtel/latiss_take_sequence.py
@@ -273,24 +273,7 @@ class LatissTakeSequence(salobj.BaseScript):
         await self.atcs.assert_all_enabled()
 
         if self.do_check_ataos_corrections:
-            # TODO (DM-38823): Move this check to ATCS method.
-            self.log.debug("Check ATAOS corrections are enabled.")
-            ataos_corrections = await self.atcs.rem.ataos.evt_correctionEnabled.aget(
-                timeout=self.atcs.fast_timeout
-            )
-
-            assert (
-                ataos_corrections.hexapod
-                and ataos_corrections.m1
-                and ataos_corrections.atspectrograph
-            ), (
-                "Not all required ATAOS corrections are enabled. "
-                "The following loops must all be closed (True), but are currently: "
-                f"Hexapod: {ataos_corrections.hexapod}, "
-                f"M1: {ataos_corrections.m1}, "
-                f"ATSpectrograph: {ataos_corrections.atspectrograph}. "
-                "Enable corrections with the ATAOS 'enableCorrection' command before proceeding.",
-            )
+            await self.atcs.assert_ataos_corrections_enabled()
 
     async def run(self):
         self.run_started = True
