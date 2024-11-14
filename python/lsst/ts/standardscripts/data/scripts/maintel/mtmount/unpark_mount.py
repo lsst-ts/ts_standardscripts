b5d662008a6889d06809e64650ab7198c0562ba1
Marco Rocchietti <marco.rocchietti@unc.edu.ar>
2024-11-14 16:28:55 -0300

    Add SAL script to unpark MTMount.

--- python/lsst/ts/standardscripts/data/scripts/maintel/mtmount/unpark_mount.py
+++ python/lsst/ts/standardscripts/data/scripts/maintel/mtmount/unpark_mount.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # This file is part of ts_standardscripts
 #
 # Developed for the LSST Telescope and Site Systems.
@@ -19,5 +20,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-from .park_mount import *
-from .unpark_mount import *
+import asyncio
+
+from lsst.ts.standardscripts.maintel.mtmount import UnparkMount
+
+asyncio.run(UnparkMount.amain())
