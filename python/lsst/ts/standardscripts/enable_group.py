6c0af013d4679b4d1ead27bd42da710482798510
Marco Rocchietti <marco.rocchietti@unc.edu.ar>
2024-12-18 19:12:30 -0300

    In EnableGroup, StandbyGroup, and OfflineGroup move implementation of the ignore feature to the `configure` method

--- python/lsst/ts/standardscripts/enable_group.py
+++ python/lsst/ts/standardscripts/enable_group.py
@@ -117,14 +117,13 @@ class EnableGroup(salobj.BaseScript, metaclass=abc.ABCMeta):
 
     async def configure(self, config):
         self.config = config
+        if hasattr(config, "ignore"):
+            self.group.disable_checks_for_components(components=config.ignore)
 
     def set_metadata(self, metadata):
         metadata.duration = 60.0
 
     async def run(self):
-        if hasattr(self.config, "ignore"):
-            self.group.disable_checks_for_components(components=self.config.ignore)
-
         overrides = (
             dict([(comp, getattr(self.config, comp, "")) for comp in self.components()])
             if self.config is not None
