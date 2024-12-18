5e5ca2d42ed00f7bb2443c9ca066d5861d774a1a
Marco Rocchietti <marco.rocchietti@unc.edu.ar>
2024-12-18 19:15:49 -0300

    Add test of the ignore feature to EnableATCS, OfflineATCS, and StandbyATCS
    In order to test the implementation of the ignore feature in the base scripts
    `EnableGroup`, `StandbyGroup` and `OfflineGroup`.

--- tests/test_auxtel_enable_atcs.py
+++ tests/test_auxtel_enable_atcs.py
@@ -38,8 +38,18 @@ class TestEnableATTCS(
     async def basic_make_script(self, index):
         self.script = EnableATTCS(index=index)
         self.atcs_mock = ATCSMock()
+        self.script._attcs.disable_checks_for_components = unittest.mock.Mock()
 
         return (self.script, self.atcs_mock)
+
+    async def test_configure_ignore(self):
+        async with self.make_script():
+            components = ["atmcs", "notcomp", "athexapod"]
+            await self.configure_script(ignore=components)
+
+            self.script._attcs.disable_checks_for_components.assert_called_once_with(
+                components=components
+            )
 
     async def test_run(self):
         async with self.make_script():
